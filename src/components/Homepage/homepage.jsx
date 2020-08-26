0a80d29f404eb5ef2b941ccd20f7717dee23b3dd
karenpinto1602 <karenpinto1602@gmail.com>
2020-08-26 23:56:30 +0530

    third commit

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import "./homepage.css";
-import car from "./images/homepage-back.jpg";
+
+const getUpper = () => {
+  return (
+    <div className="home-upper">
+      <div className="upper-grid">
+        <div className="grid-box"></div>
+        <div className="grid-box"></div>
+      </div>
+    </div>
+  );
+};
+const getLower = () => {
+  return <div></div>;
+};
 
 class Homepage extends Component {
   state = {};
   render() {
     return (
       <div className="Main">
-        <div className="home-upper">gfcbk</div>
+        <div className="home-upper">{getUpper()}</div>
         <div className="home-specials"></div>
       </div>
     );
